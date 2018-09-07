6c6da7582fcdac374434db7492657fcd23cd78e8
ca.fernando415@gmail.com <ca.fernando415@gmail.com>
2018-09-07 01:08:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ function takeANumber(katzDeli, name) {
   return "Welcome, " + name + ". You are number " + (katzDeli.indexOf(name) + 1) + " in line.";
 }
 function nowServing(katzDeliLine) {
-  if(katzDeliLine.length < 1 || katzDeliLine ===) 
+  if(katzDeliLine.length < 1 || katzDeliLine === undefined) {
+    
+  } 
   
 }
 

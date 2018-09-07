6862dc6a1a1c78893d9a9e79c60a223ba52adf00
ca.fernando415@gmail.com <ca.fernando415@gmail.com>
2018-09-07 01:07:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function takeANumber(katzDeli, name) {
   return "Welcome, " + name + ". You are number " + (katzDeli.indexOf(name) + 1) + " in line.";
 }
 function nowServing(katzDeliLine) {
+  if(katzDeliLine.is) 
   
 }
 
